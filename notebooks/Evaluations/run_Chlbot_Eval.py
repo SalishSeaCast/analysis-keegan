f2d0e63c528086ea06d84aa6d84d74592ca0e0ea
Raif-fl <flanagan.keegan16@gmail.com>
2021-02-18 11:58:54 -0800

    Moved the timeseries with full model info to tools

---
+++ notebooks/Evaluations/run_Chlbot_Eval.py
@@ -0,0 +1,44 @@
+import papermill as pm
+import re
+import os
+
+# PSF eval:
+paramlistPSF=list()
+year_range=range(2007,2019)
+for y in year_range:
+    years=[y,y+1]
+    year=y+1
+    modver='201905'
+    modver='nowcast-green.201905'
+    PATH= '/results2/SalishSea/nowcast-green.201905/'
+    saveloc='/ocean/kflanaga/MEOPAR/savedData'
+
+    paramlistPSF.append(dict(years=years,
+                            year=year,
+                            modver=modver,
+                            PATH=PATH,
+                            saveloc=saveloc))
+
+for idict in paramlistPSF:
+    newfname=f'Chlbot_Individual_year_evaluations/{idict["year"]}_Puget_Chlbot_Evaluation.ipynb'
+    print(newfname)
+    if os.path.isfile(newfname):
+        os.remove(newfname)
+    try:
+        pm.execute_notebook(
+           'Chlbot_Base_Puget_Evaluation.ipynb',
+           newfname,
+           parameters=idict
+            );
+    except:
+        print('----------------------------------------------------------')
+        print(f"WARNING Failure for in {newfname}")
+        print('----------------------------------------------------------')
+
+
+
+
+
+
+              
+           
