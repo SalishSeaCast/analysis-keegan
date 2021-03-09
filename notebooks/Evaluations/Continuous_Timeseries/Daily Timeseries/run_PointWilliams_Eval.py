abf59dbf56c74a4449368a696fa20dfa81850f6d
Raif-fl <flanagan.keegan16@gmail.com>
2021-03-09 13:04:23 -0800

    set up papermill and ran PointWilliams all years

---
+++ notebooks/Evaluations/Continuous_Timeseries/Daily Timeseries/run_PointWilliams_Eval.py
@@ -0,0 +1,40 @@
+import papermill as pm
+import re
+import os
+
+# PSF eval:
+paramlistPSF=list()
+year_range=range(2014,2020)
+for y in year_range:
+    Mooring='PointWilliams'
+    PATH= '/results2/SalishSea/nowcast-green.201905/'
+    saveloc='/ocean/kflanaga/MEOPAR/mooredData'
+
+    paramlistPSF.append(dict(saveloc=saveloc,
+                             PATH=PATH,
+                             year=y,
+                             Mooring=Mooring))
+
+for idict in paramlistPSF:
+    newfname=f'{idict["Mooring"]}/{idict["year"]}_{idict["Mooring"]}_Timeseries.ipynb'
+    print(newfname)
+    if os.path.isfile(newfname):
+        os.remove(newfname)
+    try:
+        pm.execute_notebook(
+           'PointWilliams_Base_Timeseries.ipynb',
+           newfname,
+           parameters=idict
+            );
+    except:
+        print('----------------------------------------------------------')
+        print(f"WARNING Failure for in {newfname}")
+        print('----------------------------------------------------------')
+
+
+
+
+
+
+              
+           
